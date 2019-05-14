4688303827d88908438c277eb70e50897315b68a
Ubuntu <ubuntu@ip-172-31-31-55.ap-northeast-2.compute.internal>
2019-05-14 18:23:09 +0000

    AWS에서

--- server/app.py
+++ server/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request
 from database.manager import manager
+from flask_cors import CORS
+import hashlib
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/signin", methods = ["POST", "GET"])
 def signin():
-    data = request.args.to_dict()
+    data = request.json
+    data['user_password'] = hashlib.md5(data['user_password'].encode()).hexdigest() 
+    print(data) 
     return manager.signin(data)
 
 @app.route("/login", methods = ["POST", "GET"])
