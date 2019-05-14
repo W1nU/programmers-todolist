90543d1ef0bcbde930233e4968eaacbe074c7449
seungwoo jung <jsw9808@gmail.com>
2019-05-14 23:31:19 +0900

    서버 app 파일 수정

--- server/app.py
+++ server/app.py
@@ -1,6 +1,16 @@
-from flask import Flask
-from database import user
+from flask import Flask, request
+from database.manager import manager
 
 app = Flask(__name__)
 
-@app.route("/add_user")
+@app.route("/signin", method = ["POST", "GET"])
+def signin():
+    data = request.args.to_dict()
+    return manager.signin(data)
+
+@app.route("/login", method = ["POST", "GET"])
+def login():
+    data = request.args.to_dict()
+    return manager.login(data)
+
+app.run('0.0.0.0')
