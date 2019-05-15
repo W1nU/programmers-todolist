ba59d112baf8b7f7e7b3f8f611a53635bf32b12d
seungwoo jung <jsw9808@gmail.com>
2019-05-15 15:33:16 +0900

    Indent 수정

--- server/database/access/sessiondb.py
+++ server/database/access/sessiondb.py
@@ -8,37 +8,37 @@ class rdis:
         timeout = 3600
         self.connect = Redis(ip, port)
 
-        def is_exist_session(self, id):
-            return self.connect.exists(id)
+    def is_exist_session(self, id):
+        return self.connect.exists(id)
 
-        def reset_timeout(self, id):
-            if self.is_exist_session(id) == 1:
-                self.connect.expire(id, self.timeout)
-                return '1'
-            else:
-                return '0'
+    def reset_timeout(self, id):
+        if self.is_exist_session(id) == 1:
+            self.connect.expire(id, self.timeout)
+            return '1'
+        else:
+            return '0'
 
-        def drop_session(self, id):
-            if self.is_exist_session(id) == 1:
-                self.connect.delete(id)
-                return '1'
-            else:
-                return '0'
+    def drop_session(self, id):
+        if self.is_exist_session(id) == 1:
+            self.connect.delete(id)
+            return '1'
+        else:
+            return '0'
+    
+    def create_session(self, id):
+        if self.is_exist_session(id) == 1:
+            self.drop_session(id)
+        time_now = str(time())
+        session_key  = str(uuid4())+time_now
+        self.connect.setex(id, session_key, self.timeout)
+        return session_key
 
-        def create_session(self, id):
-            if self.is_exist_session(id) == 1:
-                self.drop_session(id)
-            time_now = str(time())
-            session_key  = str(uuid4())+time_now
-            self.connect.setex(id, session_key, self.timeout)
-            return session_key
+    def id_block(self, id):
+        self.connect.setex(id, 'id blocked for a while', 300)
 
-        def id_block(self, id):
-            self.connect.setex(id, 'id blocked for a while', 300)
-
-        def open_session(self, id):
-            if self.is_exist_session(id) == 0:
-                return '0'
-            else:
-                self.reset_timeout(id)
-                return self.connect.get(id).decode('utf-8') # 세션이 있으면 세션키 리턴
+    def open_session(self, id):
+        if self.is_exist_session(id) == 0:
+            return '0'
+        else:
+            self.reset_timeout(id)
+            return self.connect.get(id).decode('utf-8') # 세션이 있으면 세션키 리턴
